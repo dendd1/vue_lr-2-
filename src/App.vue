6e23cade2f54d5514bf6f69b78df3930867b967e
Danila <dendanden74@gmail.com>
2022-12-10 09:24:40 +0300

    add comment

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@
     <div class="row">
       <div class="col">
         <h1 class="fs-3 m-3">Заполните форму:</h1>
+        <!--Форма для ввода данных-->
         <form>
 
 
@@ -13,6 +14,7 @@
               <p class="fs-5">Профессия</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода профессии-->
               <input v-model="profession" class="input-site border fs-5 p-2" type="text">
             </div>
           </div>
@@ -22,6 +24,7 @@
               <p class="fs-5">Фалимия</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода фамилии-->
               <input v-model="surname" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -29,6 +32,7 @@
 
           <div class="row">
             <div class="col-3">
+              <!--Поле для ввода имени-->
               <p class="fs-5">Имя</p>
             </div>
             <div class="col-9">
@@ -41,6 +45,7 @@
               <p class="fs-5">Отчесво</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода Отчества-->
               <input v-model="patronymic" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -50,6 +55,7 @@
               <p class="fs-5">Город</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода города-->
               <input v-model="city" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -60,6 +66,7 @@
               <p class="fs-5">Фото</p>
             </div>
             <div class="col-9">
+              <!--Поле для загрузки ссылки на фото-->
               <input v-model="photo" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -70,6 +77,7 @@
               <p class="fs-5">Телефон</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода телефона-->
               <input v-model="phone" @input="validatePhone" class="input-site border fs-5 p-2 " type="text">
               <p class="fs-5 text-danger" style="width: 400px">{{ erPhone }}</p>
             </div>
@@ -80,6 +88,7 @@
               <p class="fs-5">Почта</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода почты-->
               <input v-model="mail" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -90,6 +99,7 @@
               <p class="fs-5">Дата рождения</p>
             </div>
             <div class="col-9">
+              <!--Поле для указания даты рождения-->
               <input v-model="bDate" class="input-site border fs-5 p-2 " type="date">
             </div>
           </div>
@@ -99,6 +109,7 @@
               <p class="fs-5">О себе</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода информации о личных качествах-->
               <textarea v-model="about" class="input-site border fs-5 p-2" style="height: 200px;" maxlength="350"
                         rows="7"></textarea>
             </div>
@@ -108,6 +119,7 @@
             <div class="col-3">
               <p class="fs-5">Образование</p>
             </div>
+            <!--Поле для выбора степени образования-->
             <div class="col-9">
               <!--              <input v-model="education" class="input-site border fs-5 p-2 " type="text">-->
               <select v-model="education" class="input-site border fs-5  ">
@@ -125,6 +137,7 @@
               <p class="fs-5">Учебное заведение</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода названия учебного заведения-->
               <input v-model="institution" class="input-site border fs-5 p-2 " type="text">
             </div>
 
@@ -135,6 +148,7 @@
               <p class="fs-5">Факультет</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода названия факультета-->
               <input v-model="faculty" class="input-site border fs-5 p-2 " type="text">
             </div>
 
@@ -145,6 +159,7 @@
               <p class="fs-5">Специализация</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода названия специализации-->
               <input v-model="specialization" class="input-site border fs-5 p-2 " type="text">
             </div>
 
@@ -155,6 +170,7 @@
               <p class="fs-5">Год окончания</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода года окончания-->
               <input v-model="Year_ending" type="number" class="input-site border fs-5 p-2 " maxlength="4" min="1950" max="2099" step="1"  />
             </div>
 
@@ -170,6 +186,7 @@
               <p class="fs-5">Ключевые навыки</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода информации о ключевых навыков-->
               <textarea v-model="skills" class="input-site border fs-5 p-2" style="height: 200px;" maxlength="150"
                         rows="7"></textarea>
             </div>
@@ -180,6 +197,7 @@
               <p class="fs-5">Желаемая ЗП (Рубли)</p>
             </div>
             <div class="col-9">
+              <!--Поле для ввода информации о желаемой зп-->
               <input v-model="money" class="input-site border fs-5 p-2 " type="text">
             </div>
           </div>
@@ -187,16 +205,18 @@
 
         </form>
       </div>
-
+      <!--Структура генерации и вывода информации в резюме-->
       <div class="col">
         <h1>Ваше резюме</h1>
         <div class="row">
+          <!--Колонка с ФИО, контактами и фото пользователя -->
           <div class="col-6">
             <p class="fs-5">{{ surname }} {{ name }} {{ patronymic }} - {{ profession }}</p>
             <p class="fs-5" v-if="erPhone.length > 0">{{ mail }}</p>
             <p class="fs-5" v-else>{{ mail }} (тел.: +7{{ phone }})</p>
             <img :src="photo" class="photo img-fluid">
           </div>
+          <!--Колонка с датой рождения, городом и описанием пользователя -->
           <div class="col-6">
             <p class="fs-5">{{ bDate }}</p>
             <p class="fs-5">г. {{ city }}</p>
@@ -204,6 +224,7 @@
             <p class="fs-5">{{ about }}</p>
           </div>
         </div>
+        <!--Информация о навыках, образовании и желаемой ЗП -->
         <div class="row">
           <p class="fs-5">Навыки: {{ skills }}</p>
           <p class="fs-5">Образование: {{ education }}</p>
@@ -225,6 +246,7 @@
 export default {
   data() {
     return {
+      // Готовый присет заполнения формы
       institution: 'МГЧПУМ (бывший тракторно-космический завод)',
       faculty: 'ФРАТ (факультет рассада автоматических технологий)',
       specialization: 'бухгалтер рассады',
@@ -247,6 +269,7 @@ export default {
     }
   },
   methods: {
+    // Ф. валидации телефона, в случае неправильного ввода, выведет подсказку для правильного написания
     validatePhone() {
       this.erPhone = '';
       if (this.phone.length < 6 || this.phone.length > 10) {
